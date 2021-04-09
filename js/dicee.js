68d10ac292a7ca3179054121af2e14a2f0b3d535
Cyrus <80824990+Cyrus-A-Lacaba@users.noreply.github.com>
2021-04-09 16:02:51 +0800

    Add files via upload

--- js/dicee.js
+++ js/dicee.js
@@ -20,14 +20,26 @@ function mediaFunction(x) {
 }
 
 function getRandom6(){
-  var retval=0;
-  retval = Math.floor(Math.random() * 7);
+  return Math.floor((Math.random() * 6) + 1);
+}
 
-  if (retval == 0 || retval == 7) {retval = getRandom6();}
-  return retval;
+function drawDice(){
+
+    rndDice1 = 6;
+    rndDice2 = 6;
+    document.querySelector("#dice1").classList.remove("visible");
+    document.querySelector("#dice2").classList.remove("visible");
+    document.querySelector("#dice1gif").classList.add("visible");
+    document.querySelector("#dice2gif").classList.add("visible");
+
+    document.querySelectorAll(".btn")[0].textContent = "ROLL DICE";
+    document.querySelectorAll(".btn")[0].classList.remove("btn-danger");
+    document.querySelectorAll(".btn")[0].classList.add("btn-success");
+    showWinner(rndDice1, rndDice2);
 }
 
 function rollDice(){
+  var audio = new Audio('sounds/rolldice.wav');
 
     document.querySelector("#dice1").classList.toggle("visible");
     document.querySelector("#dice2").classList.toggle("visible");
@@ -35,52 +47,62 @@ function rollDice(){
     document.querySelector("#dice2gif").classList.toggle("visible");
 
     if (document.querySelector("#dice1").classList.item(1) == "visible"){
-      document.querySelectorAll(".btn")[0].textContent = "STOP DICE";
+      audio.STOP;
+      audio.play();
+      populateStopDice();
     }
     else {
-      document.querySelectorAll(".btn")[0].textContent = "ROLL DICE";
-      getRandomDice(1);
+      populateRollDice();
+      getRandomDice();
     }
 }
 
-function getRandomDice(blnReset){
+function populateRollDice(){
 
-  if (blnReset==0){
-    rndDice1 = 1;
-    rndDice2 = 1;
-    document.querySelector("#dice1").classList.remove("visible");
-    document.querySelector("#dice2").classList.remove("visible");
-    document.querySelector("#dice1gif").classList.add("visible");
-    document.querySelector("#dice2gif").classList.add("visible");
-    document.querySelectorAll(".btn")[0].textContent = "ROLL DICE";
-  }
-  else{
-    rndDice1 = getRandom6();
-    rndDice2 = getRandom6();
-  }
+  document.querySelectorAll(".btn")[0].textContent = "ROLL DICE";
+  document.querySelectorAll(".btn")[0].classList.remove("btn-danger");
+  document.querySelectorAll(".btn")[0].classList.add("btn-success");
+}
 
-  document.querySelector("#dice1").setAttribute("src", "images/" + "dice" + rndDice1 + ".png");
-  document.querySelector("#dice2").setAttribute("src", "images/" + "dice" + rndDice2 + ".png");
+function populateStopDice(){
+  document.querySelector("h2").textContent = "Dice Rolling!"
+  document.querySelector("#p1").classList.add("visible");
+  document.querySelector("#p2").classList.add("visible");
+
+  document.querySelectorAll(".btn")[0].textContent = "STOP DICE";
+  document.querySelectorAll(".btn")[0].classList.remove("btn-success");
+  document.querySelectorAll(".btn")[0].classList.add("btn-danger");
+}
+
+function getRandomDice(){
+  rndDice1 = getRandom6();
+  rndDice2 = getRandom6();
   showWinner(rndDice1, rndDice2);
 }
 
 function showWinner(val1, val2){
-  var audio = new Audio('sounds/applause.wav');
+  var audio = new Audio('sounds/ding.wav');
+
+  document.querySelector("#dice1").setAttribute("src", "images/" + "dice" + rndDice1 + ".png");
+  document.querySelector("#dice2").setAttribute("src", "images/" + "dice" + rndDice2 + ".png");
 
   if (val1 > val2) {
     document.querySelector("#p1").classList.remove("visible");
     document.querySelector("#p2").classList.add("visible");
+    document.querySelector("h2").textContent = "Player 1 Wins!"
     audio.STOP;
     audio.play();
   }
   else if (val2 > val1) {
     document.querySelector("#p1").classList.add("visible");
     document.querySelector("#p2").classList.remove("visible");
+    document.querySelector("h2").textContent = "Player 2 Wins!"
     audio.STOP;
     audio.play();
   }
   else {
     document.querySelector("#p1").classList.add("visible");
     document.querySelector("#p2").classList.add("visible");
+    document.querySelector("h2").textContent = "Draw!"
   }
 }
